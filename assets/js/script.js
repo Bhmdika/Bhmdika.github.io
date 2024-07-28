49b315ffa99ebb2ac58f8e4d94d72b2db8649c11
Bhmdika <bihamdika456@gmail.com>
2024-07-28 07:15:53 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".teks", {
+  strings: ["Bihamdika Putra Khadafi","Umur 16 Tahun","Sedang Belajar Java Script ","Developer Pemula"],
+  typeSpeed: 60,
+  backSpeed: 60,
+  backDelay: 1000,
+  loop: true,
+});
